b2ba30add0d5239ab260f4f7607befc194a7a64e
Maya Maulina <mayamaulina16@gmail.com>
2025-03-05 16:57:37 +0800

    update footer

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,7 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 <script src="assets/umd/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
 </body>
 
 </html>
