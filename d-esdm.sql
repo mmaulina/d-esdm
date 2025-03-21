f140388392a60d3974bcd94ee50e4777af97dc43
Muhammad Irwan Firdaus <104238625+MuhammadIrwanFirdaus@users.noreply.github.com>
2025-03-21 15:17:51 +0800

    sql

--- d-esdm.sql
+++ d-esdm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2025 at 09:21 AM
+-- Generation Time: Mar 21, 2025 at 08:16 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `d-esdm`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konten_dilihat`
+--
+
+CREATE TABLE `konten_dilihat` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `konten_id` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `konten_dilihat`
+--
+
+INSERT INTO `konten_dilihat` (`id`, `id_user`, `konten_id`, `tanggal`) VALUES
+(1, 4, 1, '2025-03-21 07:08:24'),
+(2, 4, 2, '2025-03-21 07:08:24'),
+(3, 4, 3, '2025-03-21 07:08:24'),
+(4, 4, 1, '2025-03-21 07:08:29'),
+(5, 4, 2, '2025-03-21 07:08:29'),
+(6, 4, 3, '2025-03-21 07:08:29');
 
 -- --------------------------------------------------------
 
@@ -68,6 +93,29 @@ INSERT INTO `laporan_semester` (`id`, `id_user`, `nama_perusahaan`, `parameter`,
 (9, 1, 'irwan group', 'TSP/DEBU', 'aaaaaaaaa', 'aaaaaaaaa', 'uploads/1742217160_template_surat_rekomendasi_rbb2025.docx', 'uploads/1742217160_PortofolioMuhammadIrwanFirdaus1.pdf', 'diajukan', '-'),
 (10, 1, 'irwan group', 'CO', 'aaaaaaaaa', 'baik', 'uploads/1742220771_PortofolioMuhammadIrwanFirdaus1_compressed.pdf', 'uploads/1742220771_Chapter1139.pdf', 'ditolak', 'admin lagi nggak mood'),
 (11, 2, 'maya group', 'kebisingan', 'aaaaaaaaa', 'baik', 'uploads/1742379527_AplikasiManajemenAdministrasidanKeuanganSMKN1AmuntaiBerbasisWeb.docx', 'uploads/1742379527_AplikasiManajemenAdministrasidanKeuanganSMKN1AmuntaiBerbasisWeb.pdf', 'diajukan', '-');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `jenis_konten` enum('gambar','file','link','kosong') NOT NULL,
+  `konten` varchar(225) NOT NULL,
+  `caption` text NOT NULL,
+  `tanggal` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `jenis_konten`, `konten`, `caption`, `tanggal`) VALUES
+(1, 'gambar', 'uploads/1742537822_2a3066213824b688a253585469df73fd.jpg', 'adel jkt48 memilih graduation karena alasan kesehatan hal itu membuat banyak penggemar sedih', '2025-03-21 07:17:02'),
+(2, 'file', 'uploads/1742538250_CvMuhammadIrwanFirdaus.pdf', 'ini contoh format cv yang terbaru', '2025-03-21 07:24:10'),
+(3, 'gambar', 'uploads/1742538646_7fb585eca747547b55563358add80b87.jpg', 'aaaaaaaaaaaaaaa', '2025-03-21 07:30:46');
 
 -- --------------------------------------------------------
 
@@ -150,6 +198,12 @@ INSERT INTO `users` (`id_user`, `username`, `email`, `password`, `role`, `status
 --
 
 --
+-- Indexes for table `konten_dilihat`
+--
+ALTER TABLE `konten_dilihat`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `laporan_bulanan`
 --
 ALTER TABLE `laporan_bulanan`
@@ -159,6 +213,12 @@ ALTER TABLE `laporan_bulanan`
 -- Indexes for table `laporan_semester`
 --
 ALTER TABLE `laporan_semester`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -184,6 +244,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `konten_dilihat`
+--
+ALTER TABLE `konten_dilihat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `laporan_bulanan`
 --
 ALTER TABLE `laporan_bulanan`
@@ -194,6 +260,12 @@ ALTER TABLE `laporan_bulanan`
 --
 ALTER TABLE `laporan_semester`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pembangkit`
