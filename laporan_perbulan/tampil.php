49dd427b1868b28773aa7c984d2016bef2095fc4
Muhammad Irwan Firdaus <104238625+MuhammadIrwanFirdaus@users.noreply.github.com>
2025-05-05 23:21:18 +0800

    laporan semester dan bulanan

--- laporan_perbulan/tampil.php
+++ laporan_perbulan/tampil.php
@@ -300,6 +300,10 @@ $hasprofil = $stmtCheck->fetchColumn() > 0;
                                             <a href="?page=edit_laporan_perbulan&id=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
                                             <a href="?page=hapus_laporan_perbulan&id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin menghapus?');">Hapus</a>
                                         <?php endif; ?>
+                                        <?php if ($role == 'umum' && $row['status'] == 'dikembalikan'): ?>
+                                            <a href="?page=edit_laporan_perbulan&id=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
+                                            <a href="?page=hapus_laporan_perbulan&id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin menghapus?');">Hapus</a>
+                                        <?php endif; ?>
                                     </td>
                                 </tr>
                                 <!-- Modal untuk Tolak -->
